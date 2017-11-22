d6deed6e09737f8c3af4efc5608e40e25b1fa34d
Simon de Haan <simon@praekelt.org>
2017-11-22 16:30:10 +0100

    make sure to cleanup

--- generate-js-client.sh
+++ generate-js-client.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 set -ex
-shopt -s dotglob
 
 GENERATE_RESPONSE_JSON=$(curl -X POST -H "content-type:application/json" -d '{
     "spec": {},
@@ -16,5 +15,5 @@ curl $JSON_CLIENT_ZIPPED -o client.zip
 unzip client.zip
 rm -rf docs/ src/ test/
 mv javascript-client/* .
-rmdir javascript-client
+rm -rf javascript-client
 rm client.zip
